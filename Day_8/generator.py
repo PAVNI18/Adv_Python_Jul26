4f05979fa85c8c01bfcd20bc166970ddfb65411c
Pavni Zagade <zagadepavni@gmail.com>
2026-07-19 15:29:55 +0530

    generatoreg1

---
+++ Day_8/generator.py
@@ -0,0 +1,12 @@
+# Generator
+def squares(n):
+    for i in range(1,n+1):
+        yield i**2
+
+sq = squares(5) 
+print(next(sq)) 
+print(next(sq)) 
+print(next(sq)) 
+print(next(sq)) 
+print(next(sq)) 
+print(next(sq)) 
